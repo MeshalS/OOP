d93aef87fc483994376ad6a8a5c57eb6712f30a8
MeshalS <meshal.saus@gmail.com>
2020-10-22 06:02:51 +0300

    adding to start int

--- Develop/app.js
+++ Develop/app.js
@@ -109,6 +109,7 @@ function generateInstance(instance) {
         }
     })
 }
+init();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
