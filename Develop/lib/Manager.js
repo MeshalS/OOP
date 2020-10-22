e18ec13cf98ceaf4cc67682af842a16c5136cf05
MeshalS <meshal.saus@gmail.com>
2020-10-22 03:10:48 +0300

    adding MagJs

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,23 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+class Manager extends Employee {
+    constructor(name, id, email, office) {
+        super()
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.office=office
+    }
+
+  getRole(){
+      return "Manager"
+  }
+  getOfficeNumber(){
+
+    return this.office
+
+  }
+};
+module.exports=Manager
