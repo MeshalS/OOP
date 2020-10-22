c0fdf72f5ec4f61814e454888fd899c421cbad31
MeshalS <meshal.saus@gmail.com>
2020-10-22 03:05:04 +0300

    adding EngJSClass

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Employee class
+
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super()
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.github = github;
+    }
+
+    getRole() {
+        return "Engineer"
+    }
+
+    getGithub() {
+        return this.github
+    }
+};
+module.exports = Engineer
+
+
+
+// * getName()
+// * getId()
+// * getEmail()
+// * getRole()
